a5a408676b6de5b496ac6d4ea454ce60109f0b99
TheerawutP <theerawut.p@flinkone.com>
2025-11-06 10:12:31 +0700

    change structure of data frame in CAR_STA

--- LV_1/LV_1_CAR/LV_1_CAR.ino
+++ LV_1/LV_1_CAR/LV_1_CAR.ino
@@ -35,6 +35,7 @@ uint16_t lastSVal = 0; // For HoldregSet callback
 uint16_t wstate1 = 0; // Command word for SENSOR_BOARD
 uint16_t wstate2 = 0; // Command word for AIM_BOARD
 uint16_t wstate3 = 0; // Command word for CMD_BOARD
+uint16_t IN_QUEUE = 0; // Command word for Aiming_board
 
 // Data read from peripheral slaves
 uint16_t ss_read = 0; // Status read from SENSOR_BOARD
@@ -43,6 +44,7 @@ uint16_t cmd_read =0;
 
 // Response package for the STM32 Master
 uint16_t package = 0; 
+uint16_t aiming_frame = 0; 
 
 unsigned long now = 0;
 uint32_t time_print;
@@ -52,6 +54,8 @@ uint16_t bit_s[16] = {0};
 uint16_t bit_r1[16] = {0};
 uint16_t bit_r2[16] = {0};
 uint16_t bit_r3[16] = {0};
+uint16_t bit_IN_QUEUE[16] = {0};
+
 // ---------------------- CALLBACK FUNCTIONS ----------------------
 
 /*
@@ -59,10 +63,10 @@ uint16_t bit_r3[16] = {0};
  * Logs the received value.
  */
 uint16_t cbWrite(TRegister* reg, uint16_t val) {
-  if (lastSVal != val) {
-    lastSVal = val;
-    Serial.println(String("HregSet val (from STM32):") + String(val));
-  }
+//   if (lastSVal != val) {
+//     lastSVal = val;
+//     Serial.println(String("HregSet val (from STM32):") + String(val));
+//   }
   return val;
 }
 
@@ -77,6 +81,14 @@ uint16_t cbRead(TRegister* reg, uint16_t val) {
 /*
  * Helper function to set or clear a specific bit in a 16-bit word.
  */
+
+
+void extractDataframe(uint16_t *frame, uint16_t val){
+  for(int i = 0; i<=15; i++){
+    frame[i] = (val >> i) & 0x0001;
+  }
+}
+
 void writeBit(uint16_t &value, uint8_t bit, bool state) {
     if (state) {
         value |= (1 << bit);      // set
@@ -140,13 +152,25 @@ void setup() {
   // SLAVE Register Setup for STM32 Master
   // Hreg 0x0000: Command register (Write by STM32, Read by CAR)
   RTU_SLAVE.addHreg(0x0000, 0); 
+RTU_SLAVE.onGetHreg(0x0000, cbRead); 
   RTU_SLAVE.onSetHreg(0x0000, cbWrite); 
+  
   //RTU_SLAVE.onGetHreg(0x0000, cbWrite);
   
   // Hreg 0x0001: Status register (Write by CAR, Read by STM32)
   RTU_SLAVE.addHreg(0x0001, 0);  
   //RTU_SLAVE.onSetHreg(0x0001, HoldregSet); 
   RTU_SLAVE.onGetHreg(0x0001, cbRead); 
+  RTU_SLAVE.onSetHreg(0x0001, cbWrite); 
+
+  RTU_SLAVE.addHreg(0x0002, 0);  
+  RTU_SLAVE.onGetHreg(0x0002, cbRead); 
+  RTU_SLAVE.onSetHreg(0x0002, cbWrite); 
+
+  RTU_SLAVE.addHreg(0x0003, 0);  
+  RTU_SLAVE.onGetHreg(0x0003, cbRead); 
+  RTU_SLAVE.onSetHreg(0x0003, cbWrite); 
+
 
     nextActionTime = millis();
 }
@@ -160,7 +184,7 @@ void loop() {
 
   // Read the latest command from the STM32 Master (Hreg 0)
   uint16_t val = RTU_SLAVE.Hreg(0); 
-
+  uint16_t in_queue = RTU_SLAVE.Hreg(1);
   // **Application Logic:** Map the STM32 command (val) to the slave command words (wstate1, wstate2).
   // This logic runs continuously so wstate1/wstate2 are always ready when a write state is reached.
   
@@ -193,10 +217,10 @@ void loop() {
   bit_s[7] = (val & 0x0080) != 0;
   bit_s[8] = (val & 0x0100) != 0;
   bit_s[9] = (val & 0x0200) != 0;
-  bit_s[10] = (val & 0x0400) != 0; 
-  bit_s[11] = (val & 0x0800) != 0;
-  bit_s[12] = (val & 0x1000) != 0;
-  bit_s[13] = (val & 0x2000) != 0;
+//   bit_s[10] = (val & 0x0400) != 0; 
+//   bit_s[11] = (val & 0x0800) != 0;
+//   bit_s[12] = (val & 0x1000) != 0;
+//   bit_s[13] = (val & 0x2000) != 0;
 
   //motion
   writeBit(wstate2, 0, bit_s[4]); 
@@ -213,6 +237,12 @@ void loop() {
   writeBit(wstate2, 9, bit_s[13]); 
 
   wstate3 = (ss_read>>4);
+
+  extractDataframe(bit_IN_QUEUE, in_queue);
+  for(int i = 6; i<=13; i++){
+    writeBit(wstate2, i, bit_IN_QUEUE[i-6]);
+  }
+
   
   // 2. Check if it's time for the next Master action
   if (now < nextActionTime) {
@@ -251,7 +281,6 @@ void loop() {
         node.begin(AIM_BOARD, Serial2);
         result = node.writeSingleRegister(0x0000, wstate2);
         handleMasterWriteResult(AIM_BOARD, result, wstate2, "AIM_BOARD");
-        
         // Cycle back to the start of the polling loop
         currentState = STATE_READ_CMD; 
         nextActionTime = now + DELAY_BETWEEN_SLAVES;
@@ -283,7 +312,7 @@ void loop() {
   // 4. PACK RESPONSE DATA FOR STM32 MASTER (Hreg 1)
   // Combine status from all local slaves (ss_read, aim_read) into one package for the STM32.
   package = 0; 
-
+  aiming_frame = 0;
   bit_r1[0] = (ss_read & 0x0001) != 0;   
   writeBit(package, 2, bit_r1[0]); 
   
@@ -303,11 +332,20 @@ void loop() {
   bit_r2[2] = (aim_read & 0x0004) != 0;  
   bit_r2[3] = (aim_read & 0x0008) != 0;  
   bit_r2[4] = (aim_read & 0x0010) != 0;  
-  writeBit(package, 3, bit_r2[0]); 
-  writeBit(package, 5, bit_r2[1]); 
-  writeBit(package, 6, bit_r2[2]); 
-  writeBit(package, 7, bit_r2[3]); 
-  writeBit(package, 8, bit_r2[4]); 
+  bit_r2[5] = (aim_read & 0x0020) != 0;  
+  bit_r2[6] = (aim_read & 0x0040) != 0;  
+  bit_r2[7] = (aim_read & 0x0080) != 0;  
+  bit_r2[8] = (aim_read & 0x0100) != 0;  
+  
+  writeBit(aiming_frame, 0, bit_r2[0]); 
+  writeBit(aiming_frame, 1, bit_r2[1]); 
+  writeBit(aiming_frame, 2, bit_r2[2]); 
+  writeBit(aiming_frame, 3, bit_r2[3]); 
+  writeBit(aiming_frame, 4, bit_r2[4]); 
+  writeBit(aiming_frame, 5, bit_r2[5]); 
+  writeBit(aiming_frame, 6, bit_r2[6]); 
+  writeBit(aiming_frame, 7, bit_r2[7]); 
+  writeBit(aiming_frame, 8, bit_r2[8]); 
 
   //packing data from cmd_read to package
   bit_r3[0] = (cmd_read & 0x0001) != 0;
@@ -322,13 +360,16 @@ void loop() {
   writeBit(package, 12, bit_r3[4]); 
   writeBit(package, 13, bit_r3[5]); 
 
-  RTU_SLAVE.Hreg(1, package);
-
+  RTU_SLAVE.Hreg(2, package);
+  RTU_SLAVE.Hreg(3, aiming_frame);
   if(time_print - last_time_print >= print_interval){
     last_time_print = time_print;
     Serial.println(" ");
     Serial.print("Hreg[0]: "); Serial.println(RTU_SLAVE.Hreg(0));
     Serial.print("Hreg[1]: "); Serial.println(RTU_SLAVE.Hreg(1));
+    Serial.print("Hreg[2]: "); Serial.println(RTU_SLAVE.Hreg(2));
+    Serial.print("Hreg[3]: "); Serial.println(RTU_SLAVE.Hreg(3));
+
     Serial.println(" ");   
   }
 
